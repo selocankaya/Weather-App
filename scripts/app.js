e09235c9232d510c130f91d19c22e2a65e194a36
Selocan <sagilkaya26@gmail.com>
2021-05-04 22:03:24 +0300

    use Class

--- scripts/app.js
+++ scripts/app.js
@@ -3,8 +3,9 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
 
-const updateUI = (data) => {
+const updateUI = data => {
   // destructure properties
   const { cityDets, weather } = data;
 
@@ -19,10 +20,10 @@ const updateUI = (data) => {
   `;
 
   // update the night/day & icon images
-  const iconSrc = `./img/icons/${weather.WeatherIcon}.svg`;
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
   icon.setAttribute('src', iconSrc);
 
-  const timeSrc = weather.IsDayTime ? './img/day.svg' : './img/night.svg';
+  const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
   time.setAttribute('src', timeSrc);
 
   // remove the d-none class if present
@@ -31,13 +32,7 @@ const updateUI = (data) => {
   }
 };
 
-const updateCity = async (city) => {
-  const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
-  return { cityDets, weather };
-};
-
-cityForm.addEventListener('submit', (e) => {
+cityForm.addEventListener('submit', e => {
   // prevent default action
   e.preventDefault();
 
@@ -46,16 +41,18 @@ cityForm.addEventListener('submit', (e) => {
   cityForm.reset();
 
   // update the ui with new city
-  updateCity(city)
-    .then((data) => updateUI(data))
-    .catch((err) => console.log(err));
+  forecast
+    .updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 
   // set local storage
   localStorage.setItem('city', city);
 });
 
 if (localStorage.getItem('city')) {
-  updateCity(localStorage.getItem('city'))
-    .then((data) => updateUI(data))
-    .catch((err) => console.log(err));
+  forecast
+    .updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 }
